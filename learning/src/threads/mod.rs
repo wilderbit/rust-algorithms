80d119f95e88ed8baac2a1a3014962a38b2b49aa
AbrarKhan <abrar@nitk@gmail.com>
2018-10-21 10:17:20 +0530

    learning

--- learning/src/threads/mod.rs
+++ learning/src/threads/mod.rs
@@ -1,10 +1,16 @@
-use std::thread;
-use std::time::Duration;
+use std::{sync::mpsc, thread, time::Duration};
 
-fn main() {
-    let handle = thread::spawn(|| {
+fn simple_thread() {
+    let handle1 = thread::spawn(|| {
         for i in 1..10 {
-            println!{"from spawn {}", i};
+            println! {"from handle2 {}", i};
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    let handle2 = thread::spawn(|| {
+        for i in 1..10 {
+            println! {"from handle1 {}", i};
             thread::sleep(Duration::from_millis(100));
         }
     });
@@ -14,5 +20,81 @@ fn main() {
         thread::sleep(Duration::from_millis(100))
     }
 
-    handle.join().unwrap();
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+}
+
+fn move_closure(list: &'static [i32]) {
+    thread::spawn(move || {
+        println!("{:?}", list);
+    });
+
+    thread::spawn(move || {
+        println!("{:?}", list);
+    });
+
+    thread::spawn(move || {
+        println!("{:?}", list);
+    });
+}
+
+fn message_passing_data() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("Hi");
+        tx.send(val).unwrap();
+    });
+
+    thread::spawn(|| {
+        for msg in rx {
+            println!("{}", msg);
+        }
+    });
+}
+
+fn main() {
+//    simple_thread();
+
+//    static ABC: &'static [i32] = &[1, 2, 3, 4, 5];
+//    move_closure(&ABC);
+    let (tx, rx) = mpsc::channel();
+    let (tx1, rx1) = mpsc::channel();
+    let handler1 = thread::spawn(move || {
+        let h1 = thread::spawn(|| {
+            for msg in rx1 {
+                println!("{}", msg);
+            }
+        });
+
+        let h2 = thread::spawn(move || {
+            let val = String::from("Hi");
+            loop {
+                thread::sleep_ms(100);
+                tx.send(val.clone()).unwrap();
+            }
+        });
+        h1.join().unwrap();
+        h2.join().unwrap();
+    });
+
+    let handler2 = thread::spawn(|| {
+        let h1 = thread::spawn(|| {
+            for msg in rx {
+                println!("{}", msg);
+            }
+        });
+
+        let h2 = thread::spawn(move || {
+            let val = String::from("Hello");
+            loop {
+                thread::sleep_ms(100);
+                tx1.send(val.clone()).unwrap();
+            }
+        });
+        h1.join().unwrap();
+        h2.join().unwrap();
+    });
+
+    handler1.join().unwrap();
+    handler2.join().unwrap();
 }
